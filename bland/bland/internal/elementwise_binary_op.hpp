9775bdaf6f6d8631ba8402c5c5f12095a47fbf47
Nathan West <nate.ewest@gmail.com>
2024-01-08 19:41:13 -0900

    add a bland unit test that revelead a recently-discovered bug with optimized broadcasting

--- bland/bland/internal/elementwise_binary_op.hpp
+++ bland/bland/internal/elementwise_binary_op.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <fmt/format.h>
+#include <fmt/ranges.h>
 #include "bland/ndarray.hpp"
 #include "shape_helpers.hpp"
 
@@ -25,22 +27,21 @@ ndarray elementwise_binary_op(ndarray &out, const ndarray &a, const ndarray &b) 
     const auto b_offset   = b.offsets();
     const auto out_offset = out.offsets();
 
-    // Create output array with appropriate shape and data type
-    // TODO: figure out type promotion for the return type
     auto       out_data  = out.data_ptr<Out>();
     const auto out_shape = out.shape();
 
     const auto a_strides   = compute_broadcast_strides(a.shape(), a.strides(), out_shape);
     const auto b_strides   = compute_broadcast_strides(b.shape(), b.strides(), out_shape);
     const auto out_strides = out.strides();
+    // fmt::print("Inside the op, shapes: {} = {} + {}\n", out.shape(), a.shape(), b.shape());
+    // fmt::print("Inside the op, strides: {} = {} + {}\n", out_strides, a_strides, b_strides);
 
     const auto a_shape = compute_broadcast_shape(a.shape(), out_shape);
     const auto b_shape = compute_broadcast_shape(b.shape(), out_shape);
 
     auto numel = out.numel();
 
-    constexpr int64_t LAST_DIM_UNROLL_FACTOR = 8;
-    // Current (multi-dimensional) index to read into a, b, out
+    constexpr int64_t    LAST_DIM_UNROLL_FACTOR = 8;
     std::vector<int64_t> nd_index(out_shape.size(), 0);
 
     int64_t a_index   = std::accumulate(a_offset.begin(), a_offset.end(), 0);
@@ -54,63 +55,63 @@ ndarray elementwise_binary_op(ndarray &out, const ndarray &a, const ndarray &b) 
     const int64_t b_ldim_stride   = b_strides[ndim - 1];
 
     // Main loop
-    for (; n < numel; n += LAST_DIM_UNROLL_FACTOR) {
-        auto    items_left_in_last_dim = out_shape[ndim - 1] - nd_index[ndim - 1];
-        int64_t unroll_size            = LAST_DIM_UNROLL_FACTOR;
+    int64_t unroll_size = LAST_DIM_UNROLL_FACTOR;
+    for (; n < numel; n += unroll_size) {
+        auto items_left_in_last_dim = (out_shape[ndim - 1]-1) - nd_index[ndim - 1];
+        unroll_size                 = LAST_DIM_UNROLL_FACTOR;
         if (items_left_in_last_dim >= LAST_DIM_UNROLL_FACTOR) {
-            // for (int j = 0; j < LAST_DIM_UNROLL_FACTOR; ++j) {
-            out_data[out_index + 0 * out_ldim_stride] =
-                    Op::template call<Out, A, B>(a_data[a_index + 0 * a_ldim_stride], b_data[b_index + 0 * b_ldim_stride]);
-            out_data[out_index + 1 * out_ldim_stride] =
-                    Op::template call<Out, A, B>(a_data[a_index + 1 * a_ldim_stride], b_data[b_index + 1 * b_ldim_stride]);
-            out_data[out_index + 2 * out_ldim_stride] =
-                    Op::template call<Out, A, B>(a_data[a_index + 2 * a_ldim_stride], b_data[b_index + 2 * b_ldim_stride]);
-            out_data[out_index + 3 * out_ldim_stride] =
-                    Op::template call<Out, A, B>(a_data[a_index + 3 * a_ldim_stride], b_data[b_index + 3 * b_ldim_stride]);
-            out_data[out_index + 4 * out_ldim_stride] =
-                    Op::template call<Out, A, B>(a_data[a_index + 4 * a_ldim_stride], b_data[b_index + 4 * b_ldim_stride]);
-            out_data[out_index + 5 * out_ldim_stride] =
-                    Op::template call<Out, A, B>(a_data[a_index + 5 * a_ldim_stride], b_data[b_index + 5 * b_ldim_stride]);
-            out_data[out_index + 6 * out_ldim_stride] =
-                    Op::template call<Out, A, B>(a_data[a_index + 6 * a_ldim_stride], b_data[b_index + 6 * b_ldim_stride]);
-            out_data[out_index + 7 * out_ldim_stride] =
-                    Op::template call<Out, A, B>(a_data[a_index + 7 * a_ldim_stride], b_data[b_index + 7 * b_ldim_stride]);
-            // }
+            out_data[out_index + 0 * out_ldim_stride] = Op::template call<Out, A, B>(
+                    a_data[a_index + 0 * a_ldim_stride], b_data[b_index + 0 * b_ldim_stride]);
+            out_data[out_index + 1 * out_ldim_stride] = Op::template call<Out, A, B>(
+                    a_data[a_index + 1 * a_ldim_stride], b_data[b_index + 1 * b_ldim_stride]);
+            out_data[out_index + 2 * out_ldim_stride] = Op::template call<Out, A, B>(
+                    a_data[a_index + 2 * a_ldim_stride], b_data[b_index + 2 * b_ldim_stride]);
+            out_data[out_index + 3 * out_ldim_stride] = Op::template call<Out, A, B>(
+                    a_data[a_index + 3 * a_ldim_stride], b_data[b_index + 3 * b_ldim_stride]);
+            out_data[out_index + 4 * out_ldim_stride] = Op::template call<Out, A, B>(
+                    a_data[a_index + 4 * a_ldim_stride], b_data[b_index + 4 * b_ldim_stride]);
+            out_data[out_index + 5 * out_ldim_stride] = Op::template call<Out, A, B>(
+                    a_data[a_index + 5 * a_ldim_stride], b_data[b_index + 5 * b_ldim_stride]);
+            out_data[out_index + 6 * out_ldim_stride] = Op::template call<Out, A, B>(
+                    a_data[a_index + 6 * a_ldim_stride], b_data[b_index + 6 * b_ldim_stride]);
+            out_data[out_index + 7 * out_ldim_stride] = Op::template call<Out, A, B>(
+                    a_data[a_index + 7 * a_ldim_stride], b_data[b_index + 7 * b_ldim_stride]);
         } else {
-            unroll_size = items_left_in_last_dim;
-            for (int j = 0; j < items_left_in_last_dim; ++j) {
-                out_data[out_index + j * out_ldim_stride] =
-                        Op::template call<Out, A, B>(a_data[a_index + j * a_ldim_stride], b_data[b_index + j * b_ldim_stride]);
+            unroll_size = 1+items_left_in_last_dim;
+            for (int j = 0; j < unroll_size; ++j) {
+                out_data[out_index + j * out_ldim_stride] = Op::template call<Out, A, B>(
+                        a_data[a_index + j * a_ldim_stride], b_data[b_index + j * b_ldim_stride]);
             }
         }
 
-        // Increment the multi-dimensional index
+        // ***********************
+        // ***********************
+        // NOTE TO SELF
+        // I think this indexing is wrong because it mismatches with the UNROLL_FACTOR loop increment
+        // and at best its just confusing
+        // ***********************
+        // ***********************
         for (int i = ndim - 1; i >= 0; --i) {
             if (i == ndim - 1) {
+                // This is unrolled
                 nd_index[i] += unroll_size;
                 a_index += unroll_size * a_strides[i];
                 b_index += unroll_size * b_strides[i];
                 out_index += unroll_size * out_strides[i];
             } else {
+                // This is the standard not-unrolled case
                 nd_index[i] += 1;
                 a_index += a_strides[i];
                 b_index += b_strides[i];
                 out_index += out_strides[i];
             }
-
             if (nd_index[i] < out_shape[i]) {
                 break;
-            } else if (i != ndim - 1 || n + LAST_DIM_UNROLL_FACTOR >= numel) {
-                // Reset the linear index for the current dimension
-                if (i == ndim - 1) {
-                    a_index -= (unroll_size - 1) * a_strides[i];
-                    b_index -= (unroll_size - 1) * b_strides[i];
-                    out_index -= (unroll_size - 1) * out_strides[i];
-                } else {
-                    a_index -= (a_shape[i] - 1) * a_strides[i];
-                    b_index -= (b_shape[i] - 1) * b_strides[i];
-                    out_index -= (out_shape[i] - 1) * out_strides[i];
-                }
+            } else {
+                // We've gone beyond the boundary, we'll already have done the work... so just reset
+                a_index -= (a_shape[i]) * a_strides[i];
+                b_index -= (b_shape[i]) * b_strides[i];
+                out_index -= (out_shape[i]) * out_strides[i];
                 nd_index[i] = 0;
             }
         }
@@ -134,7 +135,8 @@ struct elementwise_binary_op_impl_wrapper {
         if (a.ndim() == b.ndim()) {
             for (int64_t dim = 0; dim < a.ndim(); ++dim) {
                 if (a_shape[dim] != b_shape[dim] && a_shape[dim] != 1 && b_shape[dim] != 1) {
-                    throw std::runtime_error("elementwise_binary_op: inputs match ndim but shapes are not compatible or broadcastable");
+                    throw std::runtime_error(
+                            "elementwise_binary_op: inputs match ndim but shapes are not compatible or broadcastable");
                 }
             }
             // TODO: check if this can be broadcasted....
--- bland/tests/test_arithmetic.cpp
+++ bland/tests/test_arithmetic.cpp
@@ -5,8 +5,9 @@
 
 #include "bland_matchers.hpp"
 
-#include <fmt/core.h>
-#include <fmt/ranges.h>
+// #include <fmt/core.h>
+// #include <fmt/format.h>
+// #include <fmt/ranges.h>
 #include <bland/bland.hpp>
 
 TEST_CASE("add", "[ops][arithmetic]") {
@@ -19,20 +20,42 @@ TEST_CASE("add", "[ops][arithmetic]") {
 
             auto result = x + 10;
 
-
             REQUIRE(x.numel() == 50);
 
             REQUIRE_THAT(std::vector<float>(x.data_ptr<float>(), x.data_ptr<float>() + x.numel()),
-                         BlandWithinAbs(
-                            std::vector<float>(test_array.data_ptr<float>(), test_array.data_ptr<float>() + test_array.numel()),
-                            0.0001f));
+                         BlandWithinAbs(std::vector<float>(test_array.data_ptr<float>(),
+                                                           test_array.data_ptr<float>() + test_array.numel()),
+                                        0.0001f));
 
-            auto expected =
-                    bland::arange(10.0f, 60.0f, 1.0f, DLDataType{.code = DLDataTypeCode::kDLFloat, .bits = 32});
+            auto expected = bland::arange(10.0f, 60.0f, 1.0f, DLDataType{.code = DLDataTypeCode::kDLFloat, .bits = 32});
+            REQUIRE_THAT(std::vector<float>(result.data_ptr<float>(), result.data_ptr<float>() + result.numel()),
+                         BlandWithinAbs(std::vector<float>(expected.data_ptr<float>(),
+                                                           expected.data_ptr<float>() + expected.numel()),
+                                        0.0001f));
+        }
+        { // second operand (rhs) is broadcasted
+            auto test_array =
+                    bland::arange(0.0f, 50.0f, 1.0f, DLDataType{.code = DLDataTypeCode::kDLFloat, .bits = 32});
+
+            auto a = test_array.reshape({5, 10});
+            auto b = bland::arange(0.0f, 10.0f, 1.0f, DLDataType{.code = DLDataTypeCode::kDLFloat, .bits = 32});
+
+            auto result = a + b;
+
+            REQUIRE(a.numel() == 50);
+            REQUIRE(b.numel() == 10);
+
+            auto expected = bland::arange(10.0f, 60.0f, 1.0f, DLDataType{.code = DLDataTypeCode::kDLFloat, .bits = 32});
             REQUIRE_THAT(std::vector<float>(result.data_ptr<float>(), result.data_ptr<float>() + result.numel()),
                          BlandWithinAbs(
-                            std::vector<float>(expected.data_ptr<float>(), expected.data_ptr<float>() + expected.numel()),
-                            0.0001f));
+                                 std::vector<float>{
+                                         0,  2,  4,  6,  8,  10, 12, 14, 16, 18,
+                                         10, 12, 14, 16, 18, 20, 22, 24, 26, 28,
+                                         20, 22, 24, 26, 28, 30, 32, 34, 36, 38,
+                                         30, 32, 34, 36, 38, 40, 42, 44, 46, 48,
+                                         40, 42, 44, 46, 48, 50, 52, 54, 56, 58,
+                                 },
+                                 0.0001f));
         }
     }
     SECTION("division", "division ops") {
@@ -44,20 +67,18 @@ TEST_CASE("add", "[ops][arithmetic]") {
 
             auto result = x / 10;
 
-
             REQUIRE(x.numel() == 50);
 
             REQUIRE_THAT(std::vector<float>(x.data_ptr<float>(), x.data_ptr<float>() + x.numel()),
-                         BlandWithinAbs(
-                            std::vector<float>(test_array.data_ptr<float>(), test_array.data_ptr<float>() + test_array.numel()),
-                            0.0001f));
+                         BlandWithinAbs(std::vector<float>(test_array.data_ptr<float>(),
+                                                           test_array.data_ptr<float>() + test_array.numel()),
+                                        0.0001f));
 
-            auto expected =
-                    bland::arange(0.0f, 5.0f, 0.1f, DLDataType{.code = DLDataTypeCode::kDLFloat, .bits = 32});
+            auto expected = bland::arange(0.0f, 5.0f, 0.1f, DLDataType{.code = DLDataTypeCode::kDLFloat, .bits = 32});
             REQUIRE_THAT(std::vector<float>(result.data_ptr<float>(), result.data_ptr<float>() + result.numel()),
-                         BlandWithinAbs(
-                            std::vector<float>(expected.data_ptr<float>(), expected.data_ptr<float>() + expected.numel()),
-                            0.0001f));
+                         BlandWithinAbs(std::vector<float>(expected.data_ptr<float>(),
+                                                           expected.data_ptr<float>() + expected.numel()),
+                                        0.0001f));
         }
         { // 2D slice divide by scalar
             auto test_array =
@@ -68,17 +89,16 @@ TEST_CASE("add", "[ops][arithmetic]") {
 
             auto result = x_slice / 10;
             auto expected =
-                    bland::arange(2.0f, 52.0f, 10.f, DLDataType{.code = DLDataTypeCode::kDLFloat, .bits = 32})/10;
+                    bland::arange(2.0f, 52.0f, 10.f, DLDataType{.code = DLDataTypeCode::kDLFloat, .bits = 32}) / 10;
             REQUIRE_THAT(std::vector<float>(result.data_ptr<float>(), result.data_ptr<float>() + result.numel()),
-                         BlandWithinAbs(
-                            std::vector<float>(expected.data_ptr<float>(), expected.data_ptr<float>() + expected.numel()),
-                            0.0001f));
-            expected =
-                    bland::arange(0.0f, 50.0f, 1.0f, DLDataType{.code = DLDataTypeCode::kDLFloat, .bits = 32});
+                         BlandWithinAbs(std::vector<float>(expected.data_ptr<float>(),
+                                                           expected.data_ptr<float>() + expected.numel()),
+                                        0.0001f));
+            expected = bland::arange(0.0f, 50.0f, 1.0f, DLDataType{.code = DLDataTypeCode::kDLFloat, .bits = 32});
             REQUIRE_THAT(std::vector<float>(x.data_ptr<float>(), x.data_ptr<float>() + x.numel()),
-                         BlandWithinAbs(
-                            std::vector<float>(expected.data_ptr<float>(), expected.data_ptr<float>() + expected.numel()),
-                            0.0001f));
+                         BlandWithinAbs(std::vector<float>(expected.data_ptr<float>(),
+                                                           expected.data_ptr<float>() + expected.numel()),
+                                        0.0001f));
         }
         { // Do it again, but store the result in to the slice
             auto test_array =
@@ -92,19 +112,18 @@ TEST_CASE("add", "[ops][arithmetic]") {
             REQUIRE(x.numel() == 50);
 
             REQUIRE_THAT(std::vector<float>(x.data_ptr<float>(), x.data_ptr<float>() + x.numel()),
-                         BlandWithinAbs(
-                            std::vector<float>(test_array.data_ptr<float>(), test_array.data_ptr<float>() + test_array.numel()),
-                            0.0001f));
+                         BlandWithinAbs(std::vector<float>(test_array.data_ptr<float>(),
+                                                           test_array.data_ptr<float>() + test_array.numel()),
+                                        0.0001f));
 
             REQUIRE_THAT(std::vector<float>(x.data_ptr<float>(), x.data_ptr<float>() + x.numel()),
                          BlandWithinAbs(
-                            std::vector<float>{
-                                0,   1,  .2,  3,  4,  5,  6,  7,  8,  9,
-                                10, 11, 1.2, 13, 14, 15, 16, 17, 18, 19,
-                                20, 21, 2.2, 23, 24, 25, 26, 27, 28, 29,
-                                30, 31, 3.2, 33, 34, 35, 36, 37, 38, 39,
-                                40, 41, 4.2, 43, 44, 45, 46, 47, 48, 49,
-                            }, .0001f));
+                                 std::vector<float>{
+                                         0,  1,  .2, 3,  4,  5,   6,  7,  8,   9,  10, 11, 1.2, 13, 14, 15,  16,
+                                         17, 18, 19, 20, 21, 2.2, 23, 24, 25,  26, 27, 28, 29,  30, 31, 3.2, 33,
+                                         34, 35, 36, 37, 38, 39,  40, 41, 4.2, 43, 44, 45, 46,  47, 48, 49,
+                                 },
+                                 .0001f));
         }
         { // Slice both dims
             auto test_array =
@@ -117,15 +136,12 @@ TEST_CASE("add", "[ops][arithmetic]") {
 
             REQUIRE_THAT(std::vector<float>(x.data_ptr<float>(), x.data_ptr<float>() + x.numel()),
                          BlandWithinAbs(
-                            std::vector<float>{
-                                0,   1,  2,  3,  4,  5,  6,  7,  8,  9,
-                                10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-                                20, 2.1, 2.2, 2.3, 24, 25, 26, 27, 28, 29,
-                                30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-                                40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-                            }, .0001f));
+                                 std::vector<float>{
+                                         0,  1,  2,  3,  4,   5,   6,   7,  8,  9,  10, 11, 12, 13, 14, 15, 16,
+                                         17, 18, 19, 20, 2.1, 2.2, 2.3, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
+                                         34, 35, 36, 37, 38,  39,  40,  41, 42, 43, 44, 45, 46, 47, 48, 49,
+                                 },
+                                 .0001f));
         }
-
     }
-
 }
