7ebf6baf8ecfd16eba183aedf05f3980dba9f0e2
Nathan West <nathan@quadrature.dev>
2024-06-27 23:52:33 +0000

    wip

--- bliss/preprocess/passband_static_equalize.cpp
+++ bliss/preprocess/passband_static_equalize.cpp
@@ -85,12 +85,12 @@ bland::ndarray bliss::gen_coarse_channel_inverse(int fine_per_coarse, int num_co
     h_padded = h_padded.to("cuda:0");
     auto H = bland::fft_shift_mag_square(h_padded);
     fmt::print("Done with the fft_shift_mag_square... doing slice and reshape\n");
-    H = H.to("cpu");
+    // H = H.to("cpu");
 
     auto number_coarse_channels_contributing = H.size(0)/fine_per_coarse - 1;
     H = H.slice(bland::slice_spec{0, fine_per_coarse/2, full_res_length-fine_per_coarse/2});
     fmt::print("H_slice is like {}\n", H.shape());
-    H = H.to("cuda:0");
+    // H = H.to("cuda:0");
     H = H.reshape({number_coarse_channels_contributing, fine_per_coarse});
     fmt::print("H_slice reshaped is like {}\n", H.shape());
     // H = H.slice(bland::slice_spec{0, fine_per_coarse/2, -fine_per_coarse/2+1}).reshape({number_coarse_channels_contributing, fine_per_coarse});
