eb3b45a58cf8c253cabfde3eda5b057263621aef
Nathan West <nathan@quadrature.dev>
2024-08-13 05:20:11 +0000

    add optional altair

--- bliss/python/blissdedrift/plot_utils/__init__.py
+++ bliss/python/blissdedrift/plot_utils/__init__.py
@@ -3,4 +3,8 @@
 from ._encodings import get_hits_list
 
 from .plot_hits import plot_hits
-from .scatter_matrix import scatter_matrix_hits
+
+try:
+    from .scatter_matrix import scatter_matrix_hits
+except ImportError:
+    print("WARN: plot_utils could not import `altair`. `scatter_matrix_hits` will not be available")
