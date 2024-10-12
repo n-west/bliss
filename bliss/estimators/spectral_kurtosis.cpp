d583589289109b5593b54a384bd4f3fdab56e690
Nathan West <nathan@quadrature.dev>
2024-10-11 17:58:08 -0700

    fix spectral kurtosis d (shape parameter to gamma distribution) default value to be 2
    This exposes the `d` parameter from the generalized spectral kurtosis estimator and
    sets the default value to 2. Previously it was parameterized internally but set to 1
    to match another implementation. I believe the default value for dual-polarization spectrum
    should be 2 to give a gamma distribution with shape that matches chi-square with 4 degrees
    of freedom.

--- bliss/estimators/spectral_kurtosis.cpp
+++ bliss/estimators/spectral_kurtosis.cpp
@@ -7,37 +7,23 @@
 using namespace bliss;
 
 bland::ndarray bliss::estimate_spectral_kurtosis(const bland::ndarray &spectrum_grid, int32_t N, int32_t M, float d) {
-    fmt::print("INFO: spec kurtosis with M={} and N={}\n", M, N);
+    fmt::print("INFO: spec kurtosis with M={}, N={}, d={}\n", M, N, d);
     auto s1 = bland::square(bland::sum(spectrum_grid, {0}));
     auto s2 = bland::sum(bland::square(spectrum_grid), {0});
     auto sk = ((M * N * d + 1) / (M - 1)) * (M * (s2 / s1) - 1);
     return sk;
 }
 
-bland::ndarray bliss::estimate_spectral_kurtosis(coarse_channel &cc_data) {
+bland::ndarray bliss::estimate_spectral_kurtosis(coarse_channel &cc_data, float d) {
     const bland::ndarray spectrum_grid = cc_data.data();
 
     // 1. Compute spectral kurtosis along each channel
     auto M  = spectrum_grid.size(0);
-    auto d  = 1;
     auto Fs = std::abs(1.0 / (1e6 * cc_data.foff()));
     auto N  = std::round(cc_data.tsamp() / Fs);
 
-    auto sk = estimate_spectral_kurtosis(spectrum_grid, N, M, 1.0);
+    auto sk = estimate_spectral_kurtosis(spectrum_grid, N, M, d);
 
     return sk;
 }
 
-// bland::ndarray bliss::estimate_spectral_kurtosis(scan &fil_data) {
-//     const auto spectrum_grid = fil_data.data();
-
-//     // 1. Compute spectral kurtosis along each channel
-//     auto M  = spectrum_grid.size(0);
-//     auto d  = 1;
-//     auto Fs = std::abs(1.0 / (1e6 * fil_data.foff()));
-//     auto N  = std::round(fil_data.tsamp() / Fs);
-
-//     auto sk = estimate_spectral_kurtosis(spectrum_grid, N, M, 1.0);
-
-//     return sk;
-// }
--- bliss/flaggers/spectral_kurtosis.cpp
+++ bliss/flaggers/spectral_kurtosis.cpp
@@ -27,17 +27,16 @@ bland::ndarray bliss::flag_spectral_kurtosis(const bland::ndarray &data,
     return rfi;
 }
 
-coarse_channel bliss::flag_spectral_kurtosis(coarse_channel cc_data, float lower_threshold, float upper_threshold) {
+coarse_channel bliss::flag_spectral_kurtosis(coarse_channel cc_data, float lower_threshold, float upper_threshold, float d) {
 
     auto cc_ptr = std::make_shared<coarse_channel>(cc_data);
 
-    auto deferred_accumulated_rfi = bland::ndarray_deferred([cc_data = cc_ptr, lower_threshold, upper_threshold]() {
+    auto deferred_accumulated_rfi = bland::ndarray_deferred([cc_data = cc_ptr, lower_threshold, upper_threshold, d]() {
         bland::ndarray spectrum_grid = cc_data->data();
         bland::ndarray rfi_flags     = cc_data->mask();
 
         // 1. Compute params for SK estimate
         auto M  = spectrum_grid.size(0);
-        auto d  = 1;
         auto Fs = std::abs(1.0 / (1e6 * cc_data->foff()));
         auto N  = std::round(cc_data->tsamp() / Fs);
 
@@ -54,26 +53,26 @@ coarse_channel bliss::flag_spectral_kurtosis(coarse_channel cc_data, float lower
     return cc_data;
 }
 
-scan bliss::flag_spectral_kurtosis(scan fil_data, float lower_threshold, float upper_threshold) {
+scan bliss::flag_spectral_kurtosis(scan fil_data, float lower_threshold, float upper_threshold, float d) {
     auto number_coarse_channels = fil_data.get_number_coarse_channels();
     for (auto cc_index = 0; cc_index < number_coarse_channels; ++cc_index) {
         auto cc = fil_data.read_coarse_channel(cc_index);
-        *cc     = flag_spectral_kurtosis(*cc, lower_threshold, upper_threshold);
+        *cc     = flag_spectral_kurtosis(*cc, lower_threshold, upper_threshold, d);
     }
     return fil_data;
 }
 
 observation_target
-bliss::flag_spectral_kurtosis(observation_target observations, float lower_threshold, float upper_threshold) {
+bliss::flag_spectral_kurtosis(observation_target observations, float lower_threshold, float upper_threshold, float d) {
     for (auto &filterbank : observations._scans) {
-        filterbank = flag_spectral_kurtosis(filterbank, lower_threshold, upper_threshold);
+        filterbank = flag_spectral_kurtosis(filterbank, lower_threshold, upper_threshold, d);
     }
     return observations;
 }
 
-cadence bliss::flag_spectral_kurtosis(cadence observations, float lower_threshold, float upper_threshold) {
+cadence bliss::flag_spectral_kurtosis(cadence observations, float lower_threshold, float upper_threshold, float d) {
     for (auto &observation : observations._observations) {
-        observation = flag_spectral_kurtosis(observation, lower_threshold, upper_threshold);
+        observation = flag_spectral_kurtosis(observation, lower_threshold, upper_threshold, d);
     }
     return observations;
 }
