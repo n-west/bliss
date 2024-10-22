27ba9669f1cde1ce612383883621078deaca19d8
Nathan West <nathan@quadrature.dev>
2024-10-22 11:05:05 -0800

    test rtd docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,28 @@
 # Configuration file for the Sphinx documentation builder.
+import subprocess, os
+
+def configureDoxyfile(input_dir, output_dir):
+    with open('Doxyfile.in', 'r') as file :
+        filedata = file.read()
+
+    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+
+    with open('Doxyfile', 'w') as file:
+        file.write(filedata)
+
+# Check if we're running on Read the Docs' servers
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+breathe_projects = {}
+
+if read_the_docs_build:
+    input_dir = '../'
+    output_dir = 'build'
+    configureDoxyfile(input_dir, output_dir)
+    subprocess.call('doxygen', shell=True)
+    breathe_projects['blissdedrift'] = output_dir + '/xml'
+
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
@@ -15,7 +39,7 @@ author = 'Nathan West'
 
 extensions = ["breathe"]
 
-breathe_default_project = "bliss"
+breathe_default_project = "blissdedrift"
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
