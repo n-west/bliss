64d041b3939d082ca0e75f7297b7a4dedda19ec9
Nathan West <nathan@quadrature.dev>
2024-12-04 18:21:51 +0000

    rewrite assignment cuda op and launch with more threads
    Makes it take 2/3 time it did previously and this is called several times

--- bland/bland/ops/cuda/ops_cuda.cu
+++ bland/bland/ops/cuda/ops_cuda.cu
@@ -35,54 +35,35 @@ ndarray bland::cuda::abs(ndarray a, ndarray& out) {
  * Fill out with the value of a cast to the datatype of out.
  */
 // TODO: have a safe default if ndim > MAX_NDIM
-constexpr int ASSIGN_MAX_NDIM=4;
+constexpr int ASSIGN_MAX_NDIM = 4;
 
 template <typename Out, typename S>
-__global__ void assignment_kernel(Out* out_data, int64_t* out_shape, int64_t* out_strides,
-                                S value,
-                                int64_t ndim, int64_t numel) {
+__global__ void assignment_kernel(Out* out_data, const int64_t* out_shape, const int64_t* out_strides,
+                                  S value, int64_t ndim, int64_t numel) {
     auto worker_id = blockIdx.x * blockDim.x + threadIdx.x;
     auto grid_size = gridDim.x * blockDim.x;
 
-    extern __shared__ char smem[];
-    int64_t* sh_out_shape = reinterpret_cast<int64_t*>(smem);
-    int smem_end = sizeof(int64_t) * ndim;
+    extern __shared__ int64_t shmem[];
+    int64_t* sh_out_shape = shmem;
+    int64_t* sh_out_strides = shmem + ndim;
 
-    int64_t* sh_out_strides = reinterpret_cast<int64_t*>(smem + smem_end);
-    smem_end += sizeof(int64_t) * ndim;
-
-    for (int sh_index = threadIdx.x; sh_index < ndim * 2; sh_index += blockDim.x) {
-        if (sh_index < ndim) {
-            sh_out_shape[sh_index] = out_shape[sh_index];
-        } else if (sh_index < ndim*2) {
-            sh_out_strides[sh_index - ndim] = out_strides[sh_index - ndim];
-        }
+    // Load shape and strides into shared memory
+    if (threadIdx.x < ndim) {
+        sh_out_shape[threadIdx.x] = out_shape[threadIdx.x];
+        sh_out_strides[threadIdx.x] = out_strides[threadIdx.x];
     }
     __syncthreads();
 
     int64_t nd_index[ASSIGN_MAX_NDIM] = {0};
-    auto flattened_work_item = worker_id;
-    for (int dim = ndim - 1; dim >= 0; --dim) {
-        nd_index[dim] = flattened_work_item % sh_out_shape[dim];
-        flattened_work_item /= sh_out_shape[dim];
-    }
     for (int64_t n = worker_id; n < numel; n += grid_size) {
+        int64_t flattened_work_item = n;
         int64_t out_index = 0;
-        for (int dim = 0; dim < ndim; ++dim) {
+        for (int dim = ndim - 1; dim >= 0; --dim) {
+            nd_index[dim] = flattened_work_item % sh_out_shape[dim];
+            flattened_work_item /= sh_out_shape[dim];
             out_index += nd_index[dim] * sh_out_strides[dim];
         }
         out_data[out_index] = value;
-        auto increment_amount = grid_size;
-        for (int dim = ndim - 1; dim >= 0; --dim) {
-            nd_index[dim] += increment_amount;
-            if (nd_index[dim] < sh_out_shape[dim]) {
-                break;
-            } else {
-                // The remainder might be multiples of dim sizes
-                increment_amount = nd_index[dim] / sh_out_shape[dim];
-                nd_index[dim] = nd_index[dim] % sh_out_shape[dim];
-            }
-        }
     }
 }
 
@@ -102,7 +83,7 @@ struct assignment_op_impl_wrapper {
         int block_size = 256; // TODO: for some small numels we might still want to reduce this
         auto numel = out.numel();
         // TODO: do some benchmarking to get a better default max number of blocks
-        int num_blocks = std::min<int>(112, (numel+block_size-1) / block_size);
+        int num_blocks = std::min<int>(256, (numel+block_size-1) / block_size);
         // cudaDeviceSynchronize();
         auto smem = sizeof(int64_t) * out.ndim()*2;
         assignment_kernel<Out, S><<<num_blocks, block_size, smem>>>(out_data, thrust::raw_pointer_cast(dev_out_shape.data()), thrust::raw_pointer_cast(dev_out_strides.data()),
